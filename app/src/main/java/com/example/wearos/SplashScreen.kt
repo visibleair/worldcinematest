b39c76a8dca26657ba53af639311f16769467b8a
student <dadsa>
2022-01-27 14:16:30 +0300

    first commit

---
+++ app/src/main/java/com/example/wearos/SplashScreen.kt
@@ -0,0 +1,13 @@
+package com.example.wearos
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+}
